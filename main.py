48795812fa1aab235ecde5fa612611d1481b9cb5
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-07-13 00:05:11 +0300

    exec process and view status

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from subprocess import PIPE, Popen, run
+from subprocess import PIPE, Popen
+
+import psutil
 from psutil import Process
 from py_cui import PyCUI
 from py_cui.keys import KEY_ENTER
+
+
+class UserProcess(object):
+    def __init__(self, pid):
+        self._process = Process(pid)
+        self.name = ' '.join(self._process.cmdline())
+
+    def get_pid(self):
+        return str(self._process.pid)
+
+    def status(self):
+        return self._process.status()
+
+    @property
+    def get_name(self):
+        return self.name
 
 
 class UIManager:
     def __init__(self, master: PyCUI):
         # Get the root window
         self.master = master
+
+        # Started processes
+        self.proc_list = []
 
         # Add widgets
 
@@ -39,25 +60,41 @@ class UIManager:
     def start(self):
         self.master.start()
 
+    def check_processes_status(self):
+        if not self.proc_list:
+            return
+
+        self.menu.clear()
+        refresh = []
+        for i in range(len(self.proc_list)):
+            try:
+                self.menu.add_item(
+                    self.proc_list[i].get_name
+                    + " --- " +
+                    self.proc_list[i].status()
+                    + " --- " +
+                    self.proc_list[i].get_pid())
+                refresh.append(self.proc_list[i])
+            except psutil.NoSuchProcess:
+                pass
+        self.proc_list = refresh
+
     def execute_command(self):
         command = self.command_block.get()
         if command == "":
             return
-        output = run(command, stdout=PIPE, stderr=PIPE, text=True, shell=True)
-        self.menu.add_item(command)
+        proc = Popen(command, stdout=PIPE, stderr=PIPE, text=True, shell=True)
+        # Add new process to list
+        self.proc_list.append(UserProcess(proc.pid))
+        # Check processes status
+        self.check_processes_status()
+        # Refresh process list
         self.command_block.clear()
-        if output.returncode == 0:
-            self.output.set_text(output.stdout)
-        else:
-            self.output.set_text(output.stderr)
 
 
 root = PyCUI(9, 9)
 root.toggle_unicode_borders()
 root.set_title("Process Manager")
 
-manager = UIManager(root)
-manager.start()
-
-
-
+ui_manager = UIManager(root)
+ui_manager.start()
