8363101e0ce66803865c3aacfd7c17b8e230039c
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-07-13 16:05:34 +0300

    refactor

--- main.py
+++ main.py
@@ -1,57 +1,62 @@
 from subprocess import PIPE, Popen
-
-import py_cui.colors
 from psutil import Process, NoSuchProcess
 from py_cui import PyCUI
 from py_cui.keys import KEY_ENTER
 
 
 class UserProcess(object):
-    def __init__(self, pid):
-        self._text = None
-        self._process = Process(pid)
-        self._name = ' '.join(self._process.cmdline())
+    def __init__(self, spawn, wrap):
+        self._popen: Popen = spawn
+        self._process: Process = wrap
+
+        self._identifier = None
+        self._cmd = ' '.join(self._process.cmdline())
 
     def get_pid(self):
-        return str(self._process.pid)
+        return self._process.pid
 
-    def status(self):
+    def get_status(self):
         return self._process.status()
 
     @property
-    def get_name(self):
-        return self._name
+    def identifier(self):
+        return self._identifier
+
+    @identifier.setter
+    def identifier(self, val):
+        self._identifier = val
 
     @property
-    def text(self):
-        return self._text
+    def cmd(self):
+        return self._cmd
 
-    @text.setter
-    def text(self, text):
-        self._text = text
+    @staticmethod
+    def compute_identifier(value: str) -> int:
+        if value == "":
+            return -1
+        return sum(ord(i) for i in value)
 
 
 class UIManager:
-    def __init__(self, master: PyCUI):
+    def __init__(self, x, y):
         # Get the root window
-        self.master = master
+        self.master = PyCUI(x, y)
 
         # Started processes
-        self.proc_list = []
+        self.process_list = []
 
         # Add widgets
 
         # Process List
-        self.menu = master.add_scroll_menu(
+        self.menu = self.master.add_scroll_menu(
             "Processes",
             0, 0,
             row_span=7,
             column_span=4
         )
-        self.menu.add_key_command(KEY_ENTER, self.display_output)
 
         # Execute command
-        self.command_block = master.add_text_box(
+        self.command_block = self.master.add_text_box(
             "Exec",
             7, 0,
             row_span=1,
@@ -60,7 +65,7 @@ class UIManager:
         self.command_block.add_key_command(KEY_ENTER, self.execute_command)
 
         # Process Output
-        self.output = master.add_text_block(
+        self.output = self.master.add_text_block(
             "Output",
             0, 5,
             row_span=9,
@@ -70,57 +75,51 @@ class UIManager:
     def start(self):
         self.master.start()
 
-    def display_output(self):
-        selected = self.menu.get()
-        if selected is None:
-            return
+    def set_title(self, title):
+        self.master.set_title(title)
 
-        proc = None
-        for process in self.proc_list:
-            if selected.__eq__(process.get_name):
-                self.output.set_text(selected.text)
-                break
+    def set_unicode_border(self):
+        self.master.toggle_unicode_borders()
 
     def check_processes_status(self):
-        if not self.proc_list:
+        if not self.process_list:
             return
-
         self.menu.clear()
         refresh = []
-        for i in range(len(self.proc_list)):
+        for proc in self.process_list:
             try:
-                self.menu.add_item(
-                    self.proc_list[i].get_name
-                    + " --- " +
-                    self.proc_list[i].status()
-                    + " --- " +
-                    self.proc_list[i].get_pid())
-                refresh.append(self.proc_list[i])
+                name = f'{proc.cmd} ' \
+                       f'- {proc.get_status()} -' \
+                       f' {proc.get_pid()} '
+                self.menu.add_item(name)
+
+                identifier = UserProcess.compute_identifier(name)
+                proc.identifier = identifier
+
+                refresh.append(proc)
             except NoSuchProcess:
                 pass
-        self.proc_list = refresh
+        self.process_list = refresh
 
     def execute_command(self):
         command = self.command_block.get()
         if command == "":
             return
-        proc = Popen(command, stdout=PIPE, stderr=PIPE, text=True, shell=True)
-        wrap_proc = UserProcess(proc.pid)
-        if proc.returncode == 0:
-            wrap_proc.text = proc.stdout.readlines()
-
-
-        # Add new process to list
-        self.proc_list.append(wrap_proc)
-        # Check processes status
-        self.check_processes_status()
+        # Spawn new process with shell
+        spawn_process = Popen(command, stdout=PIPE, stderr=PIPE, text=True, shell=True)
+        # Wrap the new process in a Process object
+        wrap_process = Process(spawn_process.pid)
+        # Wrapper for proc objs
+        user_process = UserProcess(spawn_process, wrap_process)
+        # Add process management list
+        self.process_list.append(user_process)
         # Refresh process list
+        self.check_processes_status()
+        # Clear command box
         self.command_block.clear()
 
 
-root = PyCUI(9, 9)
-root.toggle_unicode_borders()
-root.set_title("Process Manager")
-
-ui_manager = UIManager(root)
+ui_manager = UIManager(9, 9)
+ui_manager.set_unicode_border()
+ui_manager.set_title("Process Manager")
 ui_manager.start()
